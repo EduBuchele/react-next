0feae64873da337aa58b506baca5733bd508ec2e
Eduardo <eduardo.buchele@gmail.com>
2021-06-06 10:42:11 -0300

    Adicionado profile e countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { CountDown } from "../components/CountDown";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import styles from '../styles/pages/Home.module.css'
+
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
 
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
+        </div>
+
+        <div></div>
+      </section>
     </div>
   )
 }
